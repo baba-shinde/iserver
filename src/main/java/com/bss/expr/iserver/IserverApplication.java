f3c5806fb7f9086a385d9a8251b115960f73564a
Baba Shinde <babashinde@Babas-MacBook-Pro.local>
2023-04-29 16:30:00 +0530

    Initial Commit

---
+++ src/main/java/com/bss/expr/iserver/IserverApplication.java
@@ -0,0 +1,13 @@
+package com.bss.expr.iserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bss/expr/iserver/IserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bss.expr.iserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
